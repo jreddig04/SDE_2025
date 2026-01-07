80abfc392e9311146fd64a49c5396e59512de61d
jreddig04 <105049594+jreddig04@users.noreply.github.com>
2026-01-07 12:05:42 +0100

    project setup

---
+++ Gamescenter/src/test/java/jreddig/gamescenter/GamescenterApplicationTests.java
@@ -0,0 +1,13 @@
+package jreddig.gamescenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamescenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
